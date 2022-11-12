c7349c0a861cfda2c767b3b359e136f6b07884b0
Shaikh Vakas Ahmed <97548328+Vakasahmed@users.noreply.github.com>
2022-11-12 12:34:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  console.log("i am getting the response");
+ fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((data) => res.send(data));
+    
+});
+
+app.get("*", (req, res) => {
+  res.send("OK")
+});
+
+app.listen(PORT, () => {
+  console.log(`BE started at port ${PORT}`);
+});
